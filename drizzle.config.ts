fa46ea451775e13973f7603e6fd63938f577c700
Hicham SADDEK <hicham@sdk.enterprises>
2026-09-06 00:51:23 +0200

    fix: auto-discover env vars with @next/env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,10 @@
+import { loadEnvConfig } from "@next/env";
 import { defineConfig } from "drizzle-kit";
+
+const projectDir = process.cwd();
+const { combinedEnv } = loadEnvConfig(projectDir);
+
+Object.assign(process.env, combinedEnv);
 
 export default defineConfig({
   schema: "./packages/db/src/schema.ts",
